aa3c5b24288b5351b3c2a4670fcefa1784daf877
tom4996131207 <tom4996131207@gmail.com>
2017-04-26 19:18:31 +0800

    Update scrape_links_from_reviewer.py

--- scrape_links_from_reviewer.py
+++ scrape_links_from_reviewer.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 20 06:37:42 2017
+Created on Tue Apr 18 15:25:00 2017
 
 @author: race020
 """
 
 import requests
 from bs4 import BeautifulSoup
-import pandas
-import time
-import csv
+import pandas as pd
 
 
+# https://www.airbnb.com.tw/users/review_page/9639741?page=1&role=guest
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-res = requests.get('https://www.airbnb.com/users/show/29169973',headers=headers)
+res = requests.get('https://www.airbnb.com.tw/users/review_page/9639741?page=1&role=guest',headers=headers)
 soup = BeautifulSoup(res.text,"lxml")
 print(soup)
 
@@ -22,16 +21,18 @@ print(soup)
 scrape links from reviewers as host
 """   
 link =[]
-
-for x in soup.findAll('div',{'class':'reviews_section as_guest space-top-3'}):
-    for y in x.findAll('div',{'class':'col-md-2 col-sm-12'}):
-        for z in y.findAll('a',{'class':'text-muted','rel':"nofollow"}):
-                print(z['href'])
-                link.append(z['href'])
+import re
+# {'class':'text-muted','rel':"nofollow"}
+for z in soup.findAll('a',{"rel":re.compile("nofollow")}):
+    print(z['href'])
+    link.append(z['href'])
 
 # Create a variable of the value of the columns
 columns = {'links_from_host': link}
 
 # Create a dataframe from the columns variable
 user_profile = pd.DataFrame(columns)
-
+user_profile['links_from_host'] = \
+            user_profile['links_from_host'].map(lambda x: x.strip('\\\" /users/show/ '))
+print(user_profile['links_from_host'])
+print('www.airbnb.com'+user_profile['links_from_host'])
